1f229d4ba9f302839ef4a7180bf787ac6d2cb87e
Mehmet KEKEÇ <43828842+Kekec46@users.noreply.github.com>
2020-12-05 23:21:18 +0300

    Add files via upload

---
+++ error.c
@@ -0,0 +1,20 @@
+start
+
+(integer) $sayi;
+(string) str;
+(integer) $sayi2;
+
+$sayi=1;
+$sayi2 : 2;
+
+eger(5<6){
+	$str	: 'T';
+} degil{
+	$str	= 'N';
+}
+
+dongu($sayi|10){
+	$str	: 'S';
+}
+
+end
---
+++ example.c
@@ -0,0 +1,48 @@
+start
+
+//comment line
+
+(integer)	$sayi1;
+(integer)	$sayi2;
+(integer)	$sayi3;
+(integer) 	$sayi4;
+(floatv)	$v_sayi;
+(karakter)	$d1;
+(karakter)	$d2;
+(karakter)	$d3;
+(string)	$str1;
+
+
+eger(4==4){
+	$d1	: 't';
+	$d2	: 'n';
+	$sayi1		: 1;
+	$sayi2		: 0;
+	$sayi3		: 4500;
+	$v_sayi	: 7,452;
+}
+
+eger(4<4){
+	$d1	: 'T';
+} degil{
+	$d1	: 'N';
+}
+
+dongu($sayi1|10){
+	$d3	: 'S';
+}
+
+// for loop end
+$sayi2++;
+$sayi3--;
+$sayi4:sayi2+sayi2;
+$sayi4:sayi2*sayi1;
+
+yazdir($sayi3);
+yazdir('hello world');
+
+$str1		: d1 ^ d2;
+
+end
+
+
